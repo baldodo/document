f5d4e0b871633ec75c5a76af42a6b10554797a5b
baldodo <baldo.dominique@gmail.com>
2023-12-21 16:57:39 +0100

    add tips

---
+++ tips/tips.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# some functions and aliases :
+#
+#  cheat: alias for 'man -7'
+#    tip: function
+#       show a tip for a subject
+#
+#       Usage : tip {some_subject}
+#       tip font
+
+export TIPS=$HOME/.local/share/tips.csv
+tip() {
+   echo "subject    command                          description"
+   echo "-------    -------                          -----------"
+   grep -i "$1" "$TIPS" | column -s ';' -t | grep -i "$1" 
+}
+alias cheat='man -7'
+
+# update the tip "database"
+test -f tips.csv && cp -i tips.csv "$TIPS"
